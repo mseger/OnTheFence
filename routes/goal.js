e522571e512b0d7623dee3dd7a2c580a41da92eb
ghooton <ghooton@ubuntu.ubuntu-domain>
2013-04-09 22:48:25 -0400

    starting dragging

--- routes/goal.js
+++ routes/goal.js
@@ -1,10 +1,9 @@
 var User = require('../models/user')
 var Goal = require('../models/goal')
-var Nail = require('../models/nail')
 
 // create a new goal by looking up user, adding goal to their goal's list
 exports.create = function(req, res){
-	var newGoal = new Goal({goal_text: req.body.goal_body, owner_key: req.session.user, nails: []});
+	var newGoal = new Goal({goal_text: req.body.goal_body, owner_key: req.session.user, nails: 0});
 	newGoal.save(function(err){
 		if(err)
 			console.log("Could not create new goal");
@@ -25,6 +24,22 @@ exports.create = function(req, res){
 	});
 }
 
+exports.addnail = function(req,res){
+	//add a nail to a post using when the div associated with it gets one dropped on it.
+	//we'll give in the goal mongo id.
+	console.log(req.body)
+	incred_goal_id = req.body.goal_id
+	incred_goal = Goal.find({_id:incred_goal_id}).exec(function (err,docs){
+		if(err)
+			return console.log("Cannot add a nail to this goal");
+		docs.nails++;
+		docs.save(function(err){
+				if(err)
+					console.log("Unable to update user's goals list");
+				res.redirect('/fence');
+	});
+  });
+}
 // display all goals in db (for debugging purposes)
 exports.display = function(req, res){
 	console.log("CURR SESSION USER ID IS: ", req.session.user.FBID);
