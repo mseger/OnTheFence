a2f46b457fb5ec2dc01a400da24dd9e9e275d96c
Velociraptor <velociraptor@students.olin.edu>
2013-04-11 23:42:45 -0400

    tesing stuff

--- routes/user.js
+++ routes/user.js
@@ -2,24 +2,25 @@ var User = require('../models/user')
 
 // login a new user, start a new session
 exports.login = function (req, res) {
-  req.facebook.api('/me', function(err, data){
-  	req.facebook.api('/me/picture?redirect=false&type=large', function(err, picData){
-	  		var existentUser = User.findOne({name: data.name}, function (err, user){
-	  			if(user){
-	  				req.session.user = user;
-	  				res.redirect('/fence');
-		  		}else{
-		  			var loggedInUser = new User({name: data.name, FBID: data.id, profPicURL: picData.data.url, goals: []});
-		  			loggedInUser.save(function (err){
-				  			if(err)
-				  				console.log("Unable to save new user.");
-				  		 	req.session.user = loggedInUser; 
-				  			res.redirect('/fence');
-		  			});
-		  		}
-		  	});
-  		});
-  });
+	res.send("blahhahahahah");
+  // req.facebook.api('/me', function(err, data){
+  // 	req.facebook.api('/me/picture?redirect=false&type=large', function(err, picData){
+	 //  		var existentUser = User.findOne({name: data.name}, function (err, user){
+	 //  			if(user){
+	 //  				req.session.user = user;
+	 //  				res.redirect('/fence');
+		//   		}else{
+		//   			var loggedInUser = new User({name: data.name, FBID: data.id, profPicURL: picData.data.url, goals: []});
+		//   			loggedInUser.save(function (err){
+		// 		  			if(err)
+		// 		  				console.log("Unable to save new user.");
+		// 		  		 	req.session.user = loggedInUser; 
+		// 		  			res.redirect('/fence');
+		//   			});
+		//   		}
+		//   	});
+  // 		});
+  // });
 };
 
 // remove all existent users from the database
